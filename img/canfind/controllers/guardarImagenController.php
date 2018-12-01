8a94b24b3f8541945098011704d8da97a8c9820c
Vegitojor <j.velazquez1985@gmail.com>
2018-12-01 12:49:42 -0300

    agrego almacenamiento de fotos

---
+++ img/canfind/controllers/guardarImagenController.php
@@ -0,0 +1,14 @@
+<?php
+
+$data = json_decode(file_get_contents("php://input"));
+
+
+$file64 = $data->imagen64;
+$nameFile = $data->nameFile;
+$idMascota = $data->idMascota;
+var_dump($idMascota);
+$file = base64_decode($file64);
+
+
+
+file_put_contents('resources/Img/Mascotas/' . $nameFile .'.jpg', $file); 

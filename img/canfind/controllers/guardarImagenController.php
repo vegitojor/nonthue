17bcb89718519d4de7e13782f69653c7b527a45b
Vegitojor <j.velazquez1985@gmail.com>
2018-12-02 12:12:17 -0300

    guardar fotos

--- img/canfind/controllers/guardarImagenController.php
+++ img/canfind/controllers/guardarImagenController.php
@@ -5,10 +5,12 @@ $data = json_decode(file_get_contents("php://input"));
 
 $file64 = $data->imagen64;
 $nameFile = $data->nameFile;
-$idMascota = $data->idMascota;
-var_dump($idMascota);
+
+
 $file = base64_decode($file64);
 
 
 
 file_put_contents('resources/Img/Mascotas/' . $nameFile .'.jpg', $file); 
+
+echo "ok";
--- img/canfind/controllers/guardarImagenEncontradoController.php
+++ img/canfind/controllers/guardarImagenEncontradoController.php
@@ -5,10 +5,11 @@ $data = json_decode(file_get_contents("php://input"));
 
 $file64 = $data->imagen64;
 $nameFile = $data->nameFile;
-$idMascota = $data->idMascota;
-var_dump($idMascota);
+
+
 $file = base64_decode($file64);
 
 
 
 file_put_contents('resources/Img/Encontrados/' . $nameFile .'.jpg', $file); 
+echo "OK";
